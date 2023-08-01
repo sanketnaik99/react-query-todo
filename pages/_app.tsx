866dfc97a361b50051f75da512ca2ce88e543ea8
Sanket Naik <sanketnaik99@gmail.com>
2023-07-31 22:55:46 -0500

    Adds form and task

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <header className="w-full text-gray-700 bg-white border-t border-gray-100 shadow-sm body-font">
+        <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+          <nav className="flex flex-wrap items-center text-base lg:w-2/5">
+            <a href="#_" className="mr-5 font-medium hover:text-gray-900">
+              Home
+            </a>
+            <a href="#_" className="mr-5 font-medium hover:text-gray-900">
+              About
+            </a>
+            <a href="#_" className="font-medium hover:text-gray-900">
+              Contact
+            </a>
+          </nav>
+        </div>
+      </header>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
